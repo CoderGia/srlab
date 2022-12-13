f70d7b1afd8d7b8c72ba8ee010fd9c498994d79e
CoderGia <nguyenhoanghuanv10@gmail.com>
2022-12-13 12:00:00 +0300

    lab1_2

--- lab1/exercises_blank.py
+++ lab1/exercises_blank.py
@@ -4,45 +4,54 @@
 # Import of modules
 import numpy as np
 from scipy.fftpack import dct
+from math import log
 
 
 def split_meta_line(line, delimiter=' '):
     # First you need to prepare split_meta_line function for meta line parsing
     # line format is "Speaker_ID Gender Path"
-    
+
     """
     :param line: lines of metadata
-    :param delimiter: delimeter
+    :param delimiter: delimiter
     :return: speaker_id: speaker IDs: gender: gender: file_path: path to file
     """
 
     ###########################################################
     # Here is your code
-
+    line = line.strip('\n')
+    speaker_id, gender, file_path = line.split(delimiter)
     ###########################################################
 
     return speaker_id, gender, file_path
 
+
 def preemphasis(signal, pre_emphasis=0.97):
-    #Here you need to preemphasis input signal with pre_emphasis coeffitient
+    # Here you need to pre_emphasis input signal with pre_emphasis coeffitient
 
     """
     :param signal: input signal
-    :param pre_emphasis: preemphasis coeffitient
+    :param pre_emphasis: pre_emphasis coeffitient
     :return: emphasized_signal: signal after pre-emphasis procedure
     """
 
     ###########################################################
     # Here is your code
-
+    emphasized_signal = np.zeros(signal.shape)
+    for i in range(len(signal)):
+        if i == 0:
+            emphasized_signal[i] = signal[i]
+        else:
+            emphasized_signal[i] = signal[i] + pre_emphasis * signal[i - 1]
     ###########################################################
 
     return emphasized_signal
 
+
 def framing(emphasized_signal, sample_rate=16000, frame_size=0.025, frame_stride=0.01):
     # Here you need to perform framing of the input signal emphasized_signal with sample rate sample_rate
     # Please use hamming windowing
-    
+
     """
     :param emphasized_signal: signal after pre-emphasis procedure
     :param sample_rate: signal sampling rate
@@ -51,27 +60,31 @@ def framing(emphasized_signal, sample_rate=16000, frame_size=0.025, frame_stride
     :return: frames: output matrix [nframes x sample_rate*frame_size]
     """
 
-    frame_length, frame_step = frame_size * sample_rate, frame_stride * sample_rate # convert from seconds to samples
+    frame_length, frame_step = frame_size * sample_rate, frame_stride * sample_rate  # convert from seconds to samples
     signal_length = len(emphasized_signal)
     frame_length = int(round(frame_length))
     frame_step = int(round(frame_step))
     num_frames = int(
-        np.ceil(float(np.abs(signal_length - frame_length)) / frame_step)) # make sure that we have at least 1 frame
+        np.ceil(float(np.abs(signal_length - frame_length)) / frame_step))  # make sure that we have at least 1 frame
 
     pad_signal_length = num_frames * frame_step + frame_length
     z = np.zeros((pad_signal_length - signal_length))
-    pad_signal = np.append(emphasized_signal, z) # pad Signal to make sure that all frames have equal number of samples without
-                                                 # truncating any samples from the original signal
+    pad_signal = np.append(emphasized_signal,
+                           z)  # pad Signal to make sure that all frames have equal number of samples without
+    # truncating any samples from the original signal
 
     ###########################################################
     # Here is your code to compute frames
-
+    hamming_window = np.hamming(frame_length)
+    frames = np.array([np.array(pad_signal[i:i + frame_length] * hamming_window) for i in
+                       range(0, len(pad_signal) - frame_length, frame_step)])
     ###########################################################
 
     return frames
 
+
 def power_spectrum(frames, NFFT=512):
-    # Here you need to compute power spectum of framed signal with NFFT fft bins number
+    # Here you need to compute power spectrum of framed signal with NFFT fft bins number
 
     """
     :param frames: framed signal
@@ -83,10 +96,11 @@ def power_spectrum(frames, NFFT=512):
 
     ###########################################################
     # Here is your code to compute pow_frames
-
+    pow_frames = np.sqrt(mag_frames)
     ###########################################################
 
     return pow_frames
+
 
 def compute_fbank_filters(nfilt=40, sample_rate=16000, NFFT=512):
     # Here you need to compute fbank filters (FBs) for special case (sample_rate & NFFT)
@@ -97,30 +111,31 @@ def compute_fbank_filters(nfilt=40, sample_rate=16000, NFFT=512):
     :param NFFT: number of fft bins in power spectrum
     :return: fbank [nfilt x (NFFT/2+1)]
     """
-    
+
     low_freq_mel = 0
     high_freq = sample_rate / 2
 
     ###########################################################
-    # Here is your code to convert Convert Hz to Mel: 
+    # Here is your code to convert Hz to Mel:
     # high_freq -> high_freq_mel
-    
-    ###########################################################
-
-    mel_points = np.linspace(low_freq_mel, high_freq_mel, nfilt + 2) # equally spaced in mel scale
+    high_freq_mel = 1125 * log(1 + high_freq / 700)
 
     ###########################################################
-    # Here is your code to convert Convert Mel to Hz: 
+
+    mel_points = np.linspace(low_freq_mel, high_freq_mel, nfilt + 2)  # equally spaced in mel scale
+
+    ###########################################################
+    # Here is your code to convert Mel to Hz:
     # mel_points -> hz_points
-    
+    hz_points = 700 * (np.exp(mel_points / 1125) - 1)
     ###########################################################
 
     bin = np.floor((NFFT + 1) * hz_points / sample_rate)
 
     fbank = np.zeros((nfilt, int(np.floor(NFFT / 2 + 1))))
     for m in range(1, nfilt + 1):
-        f_m_minus = int(bin[m - 1]) # left
-        f_m = int(bin[m])           # center
+        f_m_minus = int(bin[m - 1])  # left
+        f_m = int(bin[m])  # center
         f_m_plus = int(bin[m + 1])  # right
 
         for k in range(f_m_minus, f_m):
@@ -130,45 +145,48 @@ def compute_fbank_filters(nfilt=40, sample_rate=16000, NFFT=512):
 
     return fbank
 
+
 def compute_fbanks_features(pow_frames, fbank):
     # You need to compute fbank features using power spectrum frames and suitable fbanks filters
-    
+
     """
     :param pow_frames: framed signal power spectrum, matrix [nframes x sample_rate*frame_size]
     :param fbank: matrix of the fbank filters [nfilt x (NFFT/2+1)] where NFFT: number of fft bins in power spectrum
     :return: filter_banks_features: log mel FB energies matrix [nframes x nfilt]
     """
-    
+
     ###########################################################
     # Here is your code to compute filter_banks_features
-    
+    filter_banks_features = np.dot(pow_frames, fbank.T)
     ###########################################################
 
     filter_banks_features = np.where(filter_banks_features == 0, np.finfo(float).eps,
-                                     filter_banks_features) # numerical stability
+                                     filter_banks_features)  # numerical stability
     filter_banks_features = np.log(filter_banks_features)
 
     return filter_banks_features
 
+
 def compute_mfcc(filter_banks_features, num_ceps=20):
     # Here you need to compute MFCCs features using precomputed log mel FB energies matrix
-    
+
     """
     :param filter_banks_features: log mel FB energies matrix [nframes x nfilt]
     :param num_ceps: number of cepstral components for MFCCs
     :return: mfcc: mel-frequency cepstral coefficients (MFCCs)
     """
-    
+
     ###########################################################
     # Here is your code to compute mfcc features
-    
+    mfcc = dct(filter_banks_features, type=2, axis=1, norm='ortho')[:, 1: (num_ceps + 1)]
     ###########################################################
 
     return mfcc
 
+
 def mvn_floating(features, LC, RC, unbiased=False):
     # Here you need to do mean variance normalization of the input features
-    
+
     """
     :param features: features matrix [nframes x nfeats]
     :param LC: the number of frames to the left defining the floating
@@ -176,7 +194,7 @@ def mvn_floating(features, LC, RC, unbiased=False):
     :param unbiased: biased or unbiased estimation of normalising sigma
     :return: normalised_features: normalised features matrix [nframes x nfeats]
     """
-    
+
     nframes, dim = features.shape
     LC = min(LC, nframes - 1)
     RC = min(RC, nframes - 1)
@@ -187,10 +205,10 @@ def mvn_floating(features, LC, RC, unbiased=False):
     f = (np.r_[f[RC:], np.repeat(f[[-1]], RC, axis=0)] - np.r_[np.zeros((LC + 1, dim)), f[:-LC - 1]]) / n
     s = (np.r_[s[RC:], np.repeat(s[[-1]], RC, axis=0)] - np.r_[np.zeros((LC + 1, dim)), s[:-LC - 1]]
          ) / (n - 1 if unbiased else n) - f ** 2 * (n / (n - 1) if unbiased else 1)
-    
+
     ###########################################################
     # Here is your code to compute normalised features
-    
+    normalised_features = (features - f) / np.sqrt(s)
     ###########################################################
 
     normalised_features[s == 0] = 0
--- lab2/exercises_blank.py
+++ lab2/exercises_blank.py
@@ -10,60 +10,91 @@ from skimage.morphology import opening, closing
 
 def load_vad_markup(path_to_rttm, signal, fs):
     # Function to read rttm files and generate VAD's markup in samples
-    
+
     vad_markup = np.zeros(len(signal)).astype('float32')
-        
+
     ###########################################################
     # Here is your code
-    
+    delimiter = ' '
+    segments = []
+
+    with open(path_to_rttm, 'r') as f:
+        list_lines = f.readlines()
+    for line in list_lines:
+        l = line.strip('/n')
+        l = l.split(delimiter)
+        segments.append([int(float(l[3]) * fs), int(float(l[4]) * fs)])
+
+    # print(segments)
+
+    for i in range(len(segments)):
+        vad_markup[segments[i][0]:segments[i][0] + segments[i][1]] = 1
     ###########################################################
-    
+
     return vad_markup
+
 
 def framing(signal, window=320, shift=160):
     # Function to create frames from signal
-    
-    shape   = (int((signal.shape[0] - window)/shift + 1), window)
-    frames  = np.zeros().astype('float32')
+
+    shape = (int((signal.shape[0] - window) / shift + 1), window)
+    frames = np.zeros().astype('float32')
 
     ###########################################################
     # Here is your code
-    
+    # print(shape)
+    signal_length = len(signal)
+    num_frames = int(
+        np.ceil(float(np.abs(signal_length - window)) / shift))
+    pad_signal_length = num_frames * shift + window
+    # print(num_frames, pad_signal_length)
+    z = np.zeros((pad_signal_length - signal_length))
+    pad_signal = np.append(signal, z)
+    # hamming_window = np.hamming(frame_length)
+    square_window = scipy.signal.windows.boxcar(window)
+    frames = np.array(
+        [np.array(pad_signal[i:i + window] * square_window) for i in range(0, len(pad_signal) - window, shift)])
     ###########################################################
-    
+
     return frames
+
 
 def frame_energy(frames):
     # Function to compute frame energies
-    
+
     E = np.zeros(frames.shape[0]).astype('float32')
 
     ###########################################################
     # Here is your code
-    
+    for i in range(len(E)):
+        E[i] = sum(frames[i])
     ###########################################################
-    
+
     return E
+
 
 def norm_energy(E):
     # Function to normalize energy by mean energy and energy standard deviation
-    
+
     E_norm = np.zeros(len(E)).astype('float32')
 
     ###########################################################
     # Here is your code
-    
+    m = np.mean(E)
+    sigma = np.std(E)
+    E_norm = (E - m) / sigma
     ###########################################################
-    
+
     return E_norm
+
 
 def gmm_train(E, gauss_pdf, n_realignment):
     # Function to train parameters of gaussian mixture model
-    
+
     # Initialization gaussian mixture models
-    w     = np.array([ 0.33, 0.33, 0.33])
-    m     = np.array([-1.00, 0.00, 1.00])
-    sigma = np.array([ 1.00, 1.00, 1.00])
+    w = np.array([0.33, 0.33, 0.33])
+    m = np.array([-1.00, 0.00, 1.00])
+    sigma = np.array([1.00, 1.00, 1.00])
 
     g = np.zeros([len(E), len(w)])
     for n in range(n_realignment):
@@ -71,16 +102,26 @@ def gmm_train(E, gauss_pdf, n_realignment):
         # E-step
         ###########################################################
         # Here is your code
-
+        for i in range(len(E)):
+            for j in range(len(w)):
+                summ = 0
+                for s in range(len(m)):
+                    summ += w[s] * gauss_pdf(E[i], m[s], sigma[s])
+                g[i, j] = (w[j] * gauss_pdf(E[i], m[j], sigma[j])) / summ
         ###########################################################
 
         # M-step
         ###########################################################
         # Here is your code
-
+        M = len(E)
+        for j in range(len(w)):
+            w[j] = np.mean(g[:, j])
+            m[j] = np.sum(g[:, j] * E) / (M * w[j])
+            sigma[j] = np.sqrt(np.sum(g[:, j] * (E - m[j]) ** 2) / (M * w[j]))
         ###########################################################
-        
+
     return w, m, sigma
+
 
 def eval_frame_post_prob(E, gauss_pdf, w, m, sigma):
     # Function to estimate a posterior probability that frame isn't speech
@@ -89,67 +130,76 @@ def eval_frame_post_prob(E, gauss_pdf, w, m, sigma):
 
     ###########################################################
     # Here is your code
-
+    for i in range(len(E)):
+        for j in range(len(w)):
+            summ = 0
+            for s in range(len(m)):
+                summ += w[s] * gauss_pdf(E[i], m[s], sigma[s])
+            g0[i] = (w[0] * gauss_pdf(E[i], m[0], sigma[0])) / summ
     ###########################################################
-            
+
     return g0
+
 
 def energy_gmm_vad(signal, window, shift, gauss_pdf, n_realignment, vad_thr, mask_size_morph_filt):
     # Function to compute markup energy voice activity detector based of gaussian mixtures model
-    
+
     # Squared signal
-    squared_signal = signal**2
-    
+    squared_signal = signal ** 2
+
     # Frame signal with overlap
     frames = framing(squared_signal, window=window, shift=shift)
-    
+
     # Sum frames to get energy
     E = frame_energy(frames)
-    
+
     # Normalize the energy
     E_norm = norm_energy(E)
-    
+
     # Train parameters of gaussian mixture models
     w, m, sigma = gmm_train(E_norm, gauss_pdf, n_realignment=10)
-    
+
     # Estimate a posterior probability that frame isn't speech
     g0 = eval_frame_post_prob(E_norm, gauss_pdf, w, m, sigma)
-    
+
     # Compute real VAD's markup
     vad_frame_markup_real = (g0 < vad_thr).astype('float32')  # frame VAD's markup
 
-    vad_markup_real = np.zeros(len(signal)).astype('float32') # sample VAD's markup
+    vad_markup_real = np.zeros(len(signal)).astype('float32')  # sample VAD's markup
     for idx in range(len(vad_frame_markup_real)):
-        vad_markup_real[idx*shift:shift+idx*shift] = vad_frame_markup_real[idx]
+        vad_markup_real[idx * shift:shift + idx * shift] = vad_frame_markup_real[idx]
 
-    vad_markup_real[len(vad_frame_markup_real)*shift - len(signal):] = vad_frame_markup_real[-1]
-    
+    vad_markup_real[len(vad_frame_markup_real) * shift - len(signal):] = vad_frame_markup_real[-1]
+
     # Morphology Filters
-    vad_markup_real = closing(vad_markup_real, np.ones(mask_size_morph_filt)) # close filter
-    vad_markup_real = opening(vad_markup_real, np.ones(mask_size_morph_filt)) # open filter
-    
+    vad_markup_real = closing(vad_markup_real, np.ones(mask_size_morph_filt))  # close filter
+    vad_markup_real = opening(vad_markup_real, np.ones(mask_size_morph_filt))  # open filter
+
     return vad_markup_real
+
 
 def reverb(signal, impulse_response):
     # Function to create reverberation effect
-    
+
     signal_reverb = np.zeros(len(signal)).astype('float32')
-    
+
     ###########################################################
     # Here is your code
-    
+    signal_reverb = scipy.signal.convolve(signal, impulse_response)
     ###########################################################
-    
+
     return signal_reverb
+
 
 def awgn(signal, sigma_noise):
     # Function to add white gaussian noise to signal
-    
+
     signal_noise = np.zeros(len(signal)).astype('float32')
-    
+
     ###########################################################
     # Here is your code
-    
+    noise = np.random.normal(0, sigma_noise, len(signal))
+    signal_noise = signal + noise
     ###########################################################
-    
+
     return signal_noise
